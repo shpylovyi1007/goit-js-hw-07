0cd085e1f4c9d1328a54417ffd748096a839a6a9
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-04-14 22:12:50 +0300

    homework7.part3

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const amountInput = document.getElementsByTagName('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(amountInput.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
