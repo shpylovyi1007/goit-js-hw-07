17b941b97e9de95a9e3e9ffd6cd2fe175433ddb9
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-04-17 20:53:07 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ registerForm.addEventListener('submit', event => {
     const password = form.elements.password.value;
 
     if (email === '' || password === '') {
-        alert('All form fields must be filled in');
+        return alert('All form fields must be filled in');
     }
 
     event.preventDefault();
